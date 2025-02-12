7e5062391dda1280e2543d2b307e28ac9fb01fa6
yasin <yasin-yb@hotmail.de>
2025-02-12 22:18:24 +0100

    - Create Sudoku in Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:sudoku_solver/data/repositories/solver.dart';
 import 'package:sudoku_solver/screens/input_screen.dart';
+import 'package:sudoku_solver/screens/solution_screen.dart';
 import 'package:sudoku_solver/screens/titel_screen.dart';
 
 void main() {
@@ -12,32 +14,29 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Solver testSolver = Solver([
+      "003009267",
+      "090860000",
+      "201500400",
+      "900401080",
+      "076000130",
+      "010607009",
+      "009005603",
+      "000094010",
+      "835700900",
+    ]);
     return MaterialApp(
       title: 'Sudoku Solver',
-      initialRoute: "/",
+      /*inixtialRoute: "/",
       routes: {
         "/": (context) => const TitleScreen(),
         "/input": (context) => const InputScreen(),
-      },
+      },*/
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
         useMaterial3: true,
       ),
+      home: SolutionScreen(sudoku: testSolver),
     );
   }
 }
