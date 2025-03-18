3375e407b4c6bd921a530d696b096a59cc2e1f76
yasin <yasin-yb@hotmail.de>
2025-03-18 16:35:58 +0100

    - make first attempt of creating a form with a sudoku field

--- lib/screens/input_screen.dart
+++ lib/screens/input_screen.dart
@@ -15,71 +15,70 @@ class InputScreen extends StatefulWidget {
 class _InputScreenState extends State<InputScreen> {
   final _formKey = GlobalKey<FormState>();
 
-  RegExp regExp = RegExp(r'^[0-9]+$');
-  List<String> lines = List.filled(9, "");
+  RegExp regExp = RegExp('[0-9]');
+  List<String> fields = [];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Enter in Sudoku"),
-        backgroundColor: const Color(0xffE29A4C),
-      ),
-      backgroundColor: Colors.blue,
-      body: Center(
+    return Form(
+      key: _formKey,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Enter in Sudoku"),
+          backgroundColor: const Color(0xffE29A4C),
+        ),
+        body: Container(
+        color: Colors.blue,
+        child: Center(
           child: AspectRatio(
             aspectRatio: 1, //Sqaure grid 1:1
-            child: GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 9,
-                childAspectRatio: 1,
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 9,
+                  childAspectRatio: 1,
+                ),
+                itemCount: 81,
+                itemBuilder: (context, index) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      border: Border( //make borders of 3 x 3 Cells bigger
+                        top: BorderSide(width: (index < 9) ? 5 : 1.0, color: Colors.black),
+                        //draw 3 vertical lines left one and the two in the middle
+                        left: BorderSide(width: (index % 9 == 0 || index % 9 == 3 || index % 9 == 6)
+                            ? 5 : 1.0, color: Colors.black),
+                        right: BorderSide(width: ((index + 1) % 9 == 0) ? 5 : 1.0, color: Colors.black),
+                        //draw 3 horizontal lines: 3.row, 6.row and last row
+                        bottom: BorderSide(width: (index >= 72 || (index > 17 && index < 27)
+                            || (index > 44 && index < 54)) ? 5 : 1.0, color: Colors.black),
+                      ),
+                      color: Colors.white,
+                    ),
+                    alignment: Alignment.center,
+                    child: inputField(index),
+                  );
+                },
               ),
-              itemCount: 81,
-              itemBuilder: (context, index) {
-                return Container(
-                  decoration: BoxDecoration(
-                    border: Border( //make borders of 3 x 3 Cells bigger
-                      top: BorderSide(width: (index < 9) ? 5 : 1.0, color: Colors.black),
-                      //draw 3 vertical lines left one and the two in the middle
-                      left: BorderSide(width: (index % 9 == 0 || index % 9 == 3 || index % 9 == 6)
-                          ? 5 : 1.0, color: Colors.black),
-                      right: BorderSide(width: ((index + 1) % 9 == 0) ? 5 : 1.0, color: Colors.black),
-                      //draw 3 horizontal lines: 3.row, 6.row and last row
-                      bottom: BorderSide(width: (index >= 72 || (index > 17 && index < 27)
-                          || (index > 44 && index < 54)) ? 5 : 1.0, color: Colors.black),
-                    ),
-                    color: Colors.white,
-                  ),
-                  alignment: Alignment.center,
-                  child: TextField(
-                    keyboardType: TextInputType.number,
-                    textAlign: TextAlign.center,
-                    inputFormatters: <TextInputFormatter>[
-                      FilteringTextInputFormatter.digitsOnly
-                    ],
-                    style: const TextStyle(fontSize: 36),
-                    decoration: const InputDecoration(
-                      border: InputBorder.none,  // Removes the underline (focus border)
-                      contentPadding: EdgeInsets.zero, // Removes internal padding
-                    ),
-                  ),
-
-                );
-              },
-            ),
+            )
           )
+        )
       ),
     );
   }
 
 
 
-  TextFormField inputName(String label, int index) {
+  TextFormField inputField(int index) {
     return TextFormField(
-      decoration: InputDecoration(
-        labelText: label,
-        filled: true, // activates background
-        fillColor: const Color(0xffE29A4C),
-      ),
+        keyboardType: TextInputType.number,
+        textAlign: TextAlign.center,
+        inputFormatters: <TextInputFormatter>[
+          FilteringTextInputFormatter.digitsOnly
+        ],
+        style: const TextStyle(fontSize: 36),
+        decoration: const InputDecoration(
+          border: InputBorder.none,  // Removes the underline (focus border)
+          contentPadding: EdgeInsets.zero, // Removes internal padding
+        ),
       validator: (value) {
         if(value == null || value.isEmpty){
           return "Please enter in the line";
@@ -87,20 +86,44 @@ class _InputScreenState extends State<InputScreen> {
         if(!regExp.hasMatch(value)){
           return "Please only enter in Numbers";
         }
-        if(value.length != 9){
-          return "Please enter 9 digits";
+        if(value.length != 1){
+          return "Please only Enter in one Number per Field";
         }
-        //convert String to set to see if each character is there only once
-       /* if(value.length != value.split('').toSet().length){
-          return "Please enter each Number only once";
-        }*/
         return null;
       },
+        onSaved: (value) {
+          fields[index] = value ?? ""; // if value is null safe empty string ""
+        }
+    );
+  }
+
+  /*OLD InputField
+  TextFormField inputField(String label, int index) {
+    return TextFormField(
+        decoration: InputDecoration(
+          labelText: label,
+          filled: true, // activates background
+          fillColor: const Color(0xffE29A4C),
+        ),
+        validator: (value) {
+          if(value == null || value.isEmpty){
+            return "Please enter in the line";
+          }
+          if(!regExp.hasMatch(value)){
+            return "Please only enter in Numbers";
+          }
+          if(value.length != 9){
+            return "Please enter 9 digits";
+          }
+          //convert String to set to see if each character is there only once
+          /* if(value.length != value.split('').toSet().length){
+          return "Please enter each Number only once";
+        }*/
+          return null;
+        },
         onSaved: (value) {
           lines[index] = value ?? ""; // if value is null safe empty string ""
         }
     );
-}
-
-
+  }*/
 }
