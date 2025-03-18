5e64f661065f0544f118544a5e99ee258f58c8db
yasin <yasin-yb@hotmail.de>
2025-03-18 16:58:20 +0100

    - Make sudoku input field but scrollable

--- lib/screens/input_screen.dart
+++ lib/screens/input_screen.dart
@@ -20,48 +20,81 @@ class _InputScreenState extends State<InputScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text("Enter in Sudoku"),
-          backgroundColor: const Color(0xffE29A4C),
-        ),
-        body: Container(
-        color: Colors.blue,
-        child: Center(
-          child: AspectRatio(
-            aspectRatio: 1, //Sqaure grid 1:1
-              child: GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 9,
-                  childAspectRatio: 1,
-                ),
-                itemCount: 81,
-                itemBuilder: (context, index) {
-                  return Container(
-                    decoration: BoxDecoration(
-                      border: Border( //make borders of 3 x 3 Cells bigger
-                        top: BorderSide(width: (index < 9) ? 5 : 1.0, color: Colors.black),
-                        //draw 3 vertical lines left one and the two in the middle
-                        left: BorderSide(width: (index % 9 == 0 || index % 9 == 3 || index % 9 == 6)
-                            ? 5 : 1.0, color: Colors.black),
-                        right: BorderSide(width: ((index + 1) % 9 == 0) ? 5 : 1.0, color: Colors.black),
-                        //draw 3 horizontal lines: 3.row, 6.row and last row
-                        bottom: BorderSide(width: (index >= 72 || (index > 17 && index < 27)
-                            || (index > 44 && index < 54)) ? 5 : 1.0, color: Colors.black),
-                      ),
-                      color: Colors.white,
-                    ),
-                    alignment: Alignment.center,
-                    child: inputField(index),
-                  );
-                },
-              ),
-            )
-          )
-        )
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Enter in Sudoku"),
+        backgroundColor: const Color(0xffE29A4C),
       ),
+      body: SafeArea(
+        child: Container(
+        color: Colors.blue,
+          child: SingleChildScrollView(
+          padding: MediaQuery.of(context).orientation == Orientation.portrait
+          ? const EdgeInsets.all(24) // when in Portrait mode
+              : EdgeInsets.symmetric( // when in Landscape mode
+          vertical: 40,
+          horizontal: MediaQuery.of(context).size.width / 5,
+          ),
+            child: Column(
+              children: [
+                Form(
+                  key: _formKey,
+                  child: Center(
+                    child: AspectRatio(
+                      aspectRatio: 1, //Sqaure grid 1:1
+                      child: GridView.builder(
+                        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 9,
+                          childAspectRatio: 1,
+                        ),
+                        itemCount: 81,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            decoration: BoxDecoration(
+                              border: Border( //make borders of 3 x 3 Cells bigger
+                                top: BorderSide(width: (index < 9) ? 5 : 1.0, color: Colors.black),
+                                //draw 3 vertical lines left one and the two in the middle
+                                left: BorderSide(width: (index % 9 == 0 || index % 9 == 3 || index % 9 == 6)
+                                    ? 5 : 1.0, color: Colors.black),
+                                right: BorderSide(width: ((index + 1) % 9 == 0) ? 5 : 1.0, color: Colors.black),
+                                //draw 3 horizontal lines: 3.row, 6.row and last row
+                                bottom: BorderSide(width: (index >= 72 || (index > 17 && index < 27)
+                                    || (index > 44 && index < 54)) ? 5 : 1.0, color: Colors.black),
+                              ),
+                              color: Colors.white,
+                            ),
+                            alignment: Alignment.center,
+                            child: inputField(index),
+                          );
+                        },
+                      ),
+                    )
+                  )
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState?.validate() ?? false) {
+                      _formKey.currentState?.save();
+                      print('Form is valid. Lines: $fields');
+                    } else {
+                      print('Form is not valid.');
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xffE29A4C),
+                    foregroundColor: Colors.black,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 24, vertical: 12),
+                    textStyle: const TextStyle(fontSize: 16),
+                  ),
+                  child: const Text("Calculate Solution"),
+                ),
+              ],
+            ),
+          ),
+        )
+      )
     );
   }
 
